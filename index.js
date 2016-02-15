49a4e071bafcb09f5eafc5c5ef5fa169863b7ed3
Will <will@willacton.com>
2016-02-14 21:40:44 -0800

    basic functionality

---
+++ index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {pool, constant} from 'kefir';
+
+export function streamComponent(Component, events = []) {
+	const __eventPool = new pool();
+	const plugEvent = (e) => __eventPool.plug(constant(e));
+
+	const eventEmitters = {};
+
+	function StreamableComponent(props) {
+		const map = props.map || function (o) { return o; };
+		events.forEach((event) => {
+			eventEmitters[event] = (e) => {
+				props[event] && props[event](e);
+				plugEvent(map({ event, e }));
+			}
+		});
+
+		return (<Component {...props} {...eventEmitters} />);
+	};
+	
+	StreamableComponent.__eventStream = __eventPool.map((v) => v); // return Observable
+
+	return StreamableComponent;
+}
+
+export function getStream(StreamableComponent) {
+	return StreamableComponent.__eventStream;
+}
