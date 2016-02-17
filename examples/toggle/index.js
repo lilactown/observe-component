1984ccf9d51fed3551ee60c8da382e280f7dad3b
Will <will@willacton.com>
2016-02-17 07:25:58 -0800

    toggle example

---
+++ examples/toggle/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {streamComponent, fromComponent} from 'react-streamable';
+
+const Button = streamComponent('button', ['onClick']);
+const clickStream = fromComponent(Button);
+
+function App(props) {
+	return(
+		<div>
+			<Button>{props.text}</Button>
+		</div>
+	);
+}
+
+clickStream
+	// scan passes the previous value as the first argument
+	// to it's predicate. We initialize it with `false` (off).
+	.scan((p) => !p, false)
+	.map((x) => x ? "On" : "Off")
+	.onValue((text) =>
+		render(<App { text } />, document.getElementById('app'))
+	);
