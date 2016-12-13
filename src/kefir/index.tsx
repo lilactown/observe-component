c672f61a4db0068f27b60341f1c3d2b0706e63df
Will <will@willacton.com>
2016-12-13 11:12:19 -0800

    mappedComponent idea

--- src/kefir/index.tsx
+++ src/kefir/index.tsx
@@ -51,3 +51,52 @@ export function fromComponent(
 	}
 	return observableComponent.__eventStream;
 }
+
+/*
+	Use:
+		const ClickableLi = mappedComponent('onClick')('name', ({ name }) => <li>{name}</li>);
+		...
+		function View() {
+			return (
+				<ul>
+					{['Will', 'Jacob', 'Marie'].map((name) => 
+						<ClickableLi name={name} />
+					)}
+				</ul>
+			)
+		}
+
+		fromComponent(ClickableLi)
+			.subscribe(({ type, value: nameClicked }) => {
+				console.log(`${nameClicked} was clicked`);
+			});
+*/
+
+function mappedComponent<P>(...events) {
+	return function mappedComponentFactory(
+		propName: string,	
+		Component: Component
+	): ObservableComponent<P> {
+		const __eventPool: any = Kefir.pool();	
+
+		function plugEvent(event: ComponentEvent): void {
+			__eventPool.plug(Kefir.constant(event));
+		}
+
+		function HOC(props: any): JSX.Element {
+			function createHandler(type: string): (event) => void {
+				return function handler(event) {
+					props[type] && props[type](event);
+					plugEvent(new ComponentEvent(type, props[propName]));
+				};
+			}
+			const eventHandlers = createEventHandlers(events, createHandler);
+
+			return (<Component {...props} {...eventHandlers} />);
+		};
+		
+		(HOC as ObservableComponent<P>).__eventStream = __eventPool.map((v) => v); // return Observable
+
+		return (HOC as ObservableComponent<P>);
+	};
+}
